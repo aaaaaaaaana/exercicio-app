53a2b6e8bf41c27d8dd93f0ecac8d381756a3a40
aaaaaaaaana <rm551401@fiap.com.br>
2023-10-17 10:53:21 -0300

    alteracao no conteudo das pastas

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <>
+      <h1>HOME PAGE</h1>
+
+      <div>
+        <Link href="produtos/tenis">TENIS</Link> | 
+        <Link href="produtos/produto/tenis-preto"> TENIS PRETO</Link>
+      </div>
+    </>
+  )
+}

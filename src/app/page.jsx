9fe5644becbfddf2a9e2326ffaf59b1eca91e6fb
aaaaaaaaana <rm551401@fiap.com.br>
2023-10-19 08:27:34 -0300

    adicionando links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1>HOME PAGE</h1>
+      <h1>Home Page</h1>
 
       <div>
-        <Link href="produtos/tenis">TENIS</Link> | 
-        <Link href="produtos/produto/tenis-preto"> TENIS PRETO</Link>
+        <Link href="posts/post1">Post1</Link> | 
+        <Link href="posts/produto/tenis-preto"> Post2</Link> | 
+        <Link href="posts/post1">Post3</Link> | 
+        <Link href="posts/produto/tenis-preto"> Post4</Link> | 
       </div>
     </>
   )

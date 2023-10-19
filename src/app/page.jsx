b514381c98b615c8f472bc1a96075f3735c1802a
aaaaaaaaana <rm551401@fiap.com.br>
2023-10-19 08:51:26 -0300

    links arrumados

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,10 +7,10 @@ export default function Home() {
       <h1>Home Page</h1>
 
       <div>
-        <Link href="posts/dinamicas/post1">Post1</Link> | 
-        <Link href="posts/dinamicas/post2"> Post2</Link> | 
-        <Link href="posts/dinamicas/post3">Post3</Link> | 
-        <Link href="posts/dinamicas/post4"> Post4</Link> | 
+        <Link href="posts/dinamicas/estranho/[slug]/post1">Post1</Link> | 
+        <Link href="posts/dinamicas/estranho/[slug]/post2"> Post2</Link> | 
+        <Link href="posts/dinamicas/estranho/[slug]/post3">Post3</Link> | 
+        <Link href="posts/dinamicas/estranho/[slug]/post4"> Post4</Link> | 
       </div>
     </>
   )

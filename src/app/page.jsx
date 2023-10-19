5a7a63b4091d1d93629469360706a1cc1f718f1a
aaaaaaaaana <rm551401@fiap.com.br>
2023-10-19 08:31:00 -0300

    arrumando links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,10 +7,10 @@ export default function Home() {
       <h1>Home Page</h1>
 
       <div>
-        <Link href="posts/post1">Post1</Link> | 
-        <Link href="posts/produto/tenis-preto"> Post2</Link> | 
-        <Link href="posts/post1">Post3</Link> | 
-        <Link href="posts/produto/tenis-preto"> Post4</Link> | 
+        <Link href="posts/dinamicas/post1">Post1</Link> | 
+        <Link href="posts/dinamicas/post2"> Post2</Link> | 
+        <Link href="posts/dinamicas/post3">Post3</Link> | 
+        <Link href="posts/dinamicas/post4"> Post4</Link> | 
       </div>
     </>
   )

80c666e8d2f626647b80b572e99bee3b81f5bc24
aaaaaaaaana <rm551401@fiap.com.br>
2023-10-19 08:24:57 -0300

    pagina de erro

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+
+
+export default function NotFound() {
+  return (
+    <div>
+        <h1>OOPS !!! ERROR 404</h1>
+        <p>Sorry, the pae you are looking for does not exist</p>
+    </div>
+  )
+}

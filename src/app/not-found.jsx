d605efe6d8c054f3080125d3383a23233fb0564e
BeatrizSanti <rm551856@fiap.com.br>
2023-10-19 20:01:27 -0300

    primeiro commit

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+        <h1>OOPS !! ERROR 404</h1>
+        <p>Sorry, the page you are looking for does not exist</p>
+    </div>
+  )
+}

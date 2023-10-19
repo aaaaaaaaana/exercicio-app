0d12f534fea7635ed0e3b699c968286ec51679dd
aaaaaaaaana <rm551401@fiap.com.br>
2023-10-19 09:49:39 -0300

    adicionado css do cabecalho  e rodape

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Link from "next/link";
+import Cabecalho from '@/components/Cabecalho/Cabecalho';
+import Rodape from '@/components/Rodape/Rodape';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className={inter.className}>
+        <Cabecalho/>
         {children}
+        <Rodape/>
+        
       </body>
     </html>
   )

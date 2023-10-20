ab5a37dfc559042d30a4dfeb003385bc5b3fabe4
aaaaaaaaana <rm551401@fiap.com.br>
2023-10-19 21:38:56 -0300

    adicionando function

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
 
-
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
 
       <div>
-        <Link href="posts/dinamicas/estranho/[slug]/post1">Post1</Link> | 
-        <Link href="posts/dinamicas/estranho/[slug]/post2"> Post2</Link> | 
-        <Link href="posts/dinamicas/estranho/[slug]/post3">Post3</Link> | 
-        <Link href="posts/dinamicas/estranho/[slug]/post4"> Post4</Link> | 
+        <Link href="/posts/dinamicas/estranho/post1">Post1</Link> | 
+        <Link href="/posts/dinamicas/estranho/post2"> Post2</Link> | 
+        <Link href="/posts/dinamicas/estranho/post3">Post3</Link> | 
+        <Link href="/posts/dinamicas/estranho/post4"> Post4</Link> | 
       </div>
     </>
   )

f25a9882e68249d097f9fda20434e73f32d5194f
M1relly <rm98672@fiap.com.br>
2023-10-20 22:43:54 -0300

    Page do Post3

--- src/app/posts/dinamicas/estranho/[slug]/post3/page.jsx
+++ src/app/posts/dinamicas/estranho/[slug]/post3/page.jsx
@@ -1,8 +1,25 @@
-
+import Link from "next/link";
+import Image from 'next/image'
 
 export default function Post3() {
     return (
-      <div>page</div>
+      <div>
+        <h1>Post 3</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic inventore expedita facere 
+           quis officiis minus vel nulla nisi possimus aliquid ex, odio modi eos est recusandae? 
+           Quam accusantium ducimus ad.
+        </p>
+
+      <p><Link href = "/">VOLTAR</Link></p>
+
+      <figure>
+        <Image src="/images/post3.png"
+        width={500}
+        height={500}
+        alt="Post3"
+        />
+      </figure>
+
+      </div>
     )
   }
-  

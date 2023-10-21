ac0d3d8245dbc59035c4c1a71a356a9ecdf06d08
BeatrizSanti <rm551856@fiap.com.br>
2023-10-21 14:30:35 -0300

    Add conteúdo

--- src/app/posts/dinamicas/estranho/[slug]/post2/page.jsx
+++ src/app/posts/dinamicas/estranho/[slug]/post2/page.jsx
@@ -1,2 +1,17 @@
 import Link from "next/link";
 import Image from 'next/image'
+ 
+export default function Post2() {
+    return (
+      <div>
+        <h1>Post </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic inventore expedita facere 
+           quis officiis minus vel nulla nisi possimus aliquid ex, odio modi eos est recusandae? 
+           Quam accusantium ducimus ad.
+        </p>
+
+      
+
+      </div>
+    )
+  }

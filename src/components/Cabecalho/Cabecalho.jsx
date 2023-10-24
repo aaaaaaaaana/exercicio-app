8d98b8ed3157694223f6964f9d3af4b50ba2ac2b
aaaaaaaaana <rm551401@fiap.com.br>
2023-10-23 22:31:20 -0300

    arrumando cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -6,11 +6,11 @@ export default function Cabecalho() {
     <>
         <header className="bg-teal-900 text-white h-16">
             <nav className="flex flex-row justify-around justify-items-center">
-            <Link className='linkCabecalho' href="/item/camisa">Camisa</Link> | 
-            <Link className='linkCabecalho' href="/item/calca">Calca</Link> | 
-            <Link className='linkCabecalho' href="/item/sapato">Sapato</Link> | 
-            <Link className='linkCabecalho' href="/item/oculos">Oculos</Link> | 
-            <Link className='linkCabecalho' href="/item/bone">Bone</Link>
+            <Link className='linkCabecalho' href="/produto/item/camisa">Camisa</Link> | 
+            <Link className='linkCabecalho' href="/produto/item/calca">Calca</Link> | 
+            <Link className='linkCabecalho' href="/produto/item/sapato">Sapato</Link> | 
+            <Link className='linkCabecalho' href="/produto/item/oculos">Oculos</Link> | 
+            <Link className='linkCabecalho' href="/produto/item/bone">Bone</Link>
             </nav>
         </header>
     </>

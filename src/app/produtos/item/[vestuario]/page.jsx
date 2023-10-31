e85061c44c5a79572c4fe9d8c70a9ad4c91a957a
aaaaaaaaana <rm551401@fiap.com.br>
2023-10-31 08:36:19 -0300

    arrumando conteidp

--- src/app/produtos/item/[vestuario]/page.jsx
+++ src/app/produtos/item/[vestuario]/page.jsx
@@ -1,26 +1,19 @@
-'use client'
+import React from 'react' 
+import Image from "next/image";
+import Link from 'next/link';
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
 
-const produtos = {
-    calca: '/img/calca.jpg',
-    camisa: '/img/camisa.jpg',
-    bone: '/img/bone.jpg',
-    oculos: '/img/oculos.jpg',
-    sapato: '/img/sapato.jpg'
+export default function Vestuario({params}) {
+  return (
+    
+    <div>
+        <figure className='flex flex-col items-center justify-center' >
+            <Image src={`/img/${params.vestuario}.jpg`} alt="Produtos" width={320} height={320}></Image>
+            <h1 className='text-emerald-800 font-bold text-2xl'>{params.vestuario}</h1>
+            <Link href="/">Voltar...</Link>
+        </figure>
+
+    </div>
+  
+  )
 }
-
-export default function Produto() {
-    const router = useRouter();
-    const { item } = router.query;
-
-    const imagem = produtos[item];
-    return (
-        <div>
-            <h2>{item}</h2>
-            <img src={imagem} alt={item} />
-        </div>
-    );
-}
-
--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function Cabecalho() {
   return (
-    <>
+    <div>
         <header className="bg-teal-900 text-white h-16">
             <nav className="flex flex-row justify-around justify-items-center">
-            <Link className='linkCabecalho' href="/produto/item/camisa">Camisa</Link> | 
-            <Link className='linkCabecalho' href="/produto/item/calca">Calca</Link> | 
-            <Link className='linkCabecalho' href="/produto/item/sapato">Sapato</Link> | 
-            <Link className='linkCabecalho' href="/produto/item/oculos">Oculos</Link> | 
-            <Link className='linkCabecalho' href="/produto/item/bone">Bone</Link>
+            <Link className='linkCabecalho' href="/produtos/item/camisa">Camisa</Link> | 
+            <Link className='linkCabecalho' href="/produtos/item/calca">Calca</Link> | 
+            <Link className='linkCabecalho' href="/produtos/item/sapato">Sapato</Link> | 
+            <Link className='linkCabecalho' href="/produtos/item/oculos">Oculos</Link> | 
+            <Link className='linkCabecalho' href="/produtos/item/bone">Bone</Link>
             </nav>
         </header>
-    </>
+    </div>
   )
 }
